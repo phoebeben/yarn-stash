fe5e2b27bea1ea2fee19ba62cb811a537d513f12
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-06-29 11:22:53 +0100

    added validations and created form for adding yarn

--- app/controllers/yarns_controller.rb
+++ app/controllers/yarns_controller.rb
@@ -2,4 +2,12 @@ class YarnsController < ApplicationController
   def new
     @yarn = Yarn.new
   end
+
+  def create
+    @yarn = Yarn.new(yarn_params)
+  end
+
+  def yarn_params
+    params.require(:yarn).permit(:name, :brand, :fibre_type, :yarn_weight, :skein_count, :weight, :yardage )
+  end
 end
--- app/models/yarn.rb
+++ app/models/yarn.rb
@@ -1,3 +1,7 @@
 class Yarn < ApplicationRecord
   belongs_to :user
+  belongs_to :project, through: :assigned_yarn
+
+  validates :name, presence: true
+  validates :skein_count, presence: true
 end
