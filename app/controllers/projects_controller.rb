62e5305e56a497094bb90df95e1e38975d917da7
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-07-12 12:32:51 +0100

    added functionality for multiple selections of yarn

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,14 +7,26 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     @project.user = current_user
-    @yarns = Yarn.find(params[:project][:yarn_ids])
+    selected_yarns = params[:project][:yarn_ids]
     if @project.save
-      @assigned_yarns = AssignedYarn.new(project_id: @project.id, yarn_id: @yarns.id, quantity: params[:project][:end_date])
-      @assigned_yarns.save
+      if selected_yarns.length == 1
+        yarn_objects = Yarn.find(selected_yarns)
+        create_assigned_yarn(yarn_objects)
+      else
+        yarn_objects = selected_yarns.map { |yarn| Yarn.find(yarn) }
+        yarn_objects.each do |yarn|
+          create_assigned_yarn(yarn)
+        end
+      end
       redirect_to inventory_path(@project)
     else
       render 'projects/new', status: :unprocessable_entity
     end
+  end
+
+  def create_assigned_yarn(assigned_yarn)
+    new_yarn = AssignedYarn.new(project_id: @project.id, yarn_id: assigned_yarn.id, quantity: params[:project][:end_date])
+    new_yarn.save
   end
 
   def project_params
