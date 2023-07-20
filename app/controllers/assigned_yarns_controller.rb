a9869c7034107236de803194fe51c0600a5e5b15
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-07-20 14:32:45 +0100

    assigned_yarns are saving to database

--- app/controllers/assigned_yarns_controller.rb
+++ app/controllers/assigned_yarns_controller.rb
@@ -6,11 +6,30 @@ class AssignedYarnsController < ApplicationController
 
   def create
     @project = Project.find(params[:project_id])
-    @assigned_yarn = AssignedYarn.new
+    @assigned_yarn = AssignedYarn.new(assigned_yarn_params)
+    @assigned_yarn.project_id = @project.id
+    if @assigned_yarn.save
+      add_more_check
+    else
+      render 'assigned_yarns/new', status: :unprocessable_entity
+    end
+  end
+
+  def assigned_yarn_params
+    params.require(:assigned_yarn).permit(:yarn_id, :quantity)
+  end
+
+  def add_more_check
     if params[:commit] == "Add more yarn"
       redirect_to new_project_assigned_yarn_path, locals: { project: @project, assigned_yarn: @assigned_yarn }
     else
-      raise
+      redirect_to projects_path
+    end
+  end
+
+  def check_quantity
+    AssignedYarn.where(project_id: project.id).each do |yarn|
+      Yarn.find(yarn.id)
     end
   end
 end
